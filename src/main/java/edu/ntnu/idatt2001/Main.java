0bbc7b580bd29ea6efbf9641ab12cb1e82c0d595
eliastt <eliastt@stud.ntnu.no>
2023-03-08 12:30:53 +0100

    first commit

--- src/main/java/edu/ntnu/idatt2001/Main.java
+++ src/main/java/edu/ntnu/idatt2001/Main.java
@@ -2,6 +2,6 @@ package edu.ntnu.idatt2001;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Card game!");
+        System.out.println("Card game yuppie!");
     }
 }
